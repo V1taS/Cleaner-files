9bfefaa8dcb764ca9a415d03b45dbe7049bd74cf
Sosin Vitalii <vitaliy.sosin@domrf.ru>
2021-10-21 17:43:38 +0300

    Initial Commit

---
+++ Cleaner files/UserStories/MainCleaner/Module/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Cleaner files
+//
+//  Created by Vitalii Sosin on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .yellow
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Cleaner files
+
+
